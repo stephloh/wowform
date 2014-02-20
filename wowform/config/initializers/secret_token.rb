b0bddea64cc75a2cbb65e3ee0bac5fdf18da9577
Stephanie Loh <sloh@booster.com>
2014-02-20 17:20:53 +0800

    Initial commit

---
+++ wowform/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wowform::Application.config.secret_token = '12efe952bc2704e9153975e0491ba4c9d105bab483f5dcb0f3487fb76e5ee13c37316c3b65d33f8e3d261db8bd42da22466c6cfd6300d8316758239450fc86a3'
